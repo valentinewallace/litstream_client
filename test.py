8c746387eb0a99ae8a21144dd3bb5e1a00984168
Valentine Wallace <valentine.m.wallace@gmail.com>
2018-02-26 13:44:38 -0800

    Integrate travis

---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+
+class ClientTestCase(unittest.TestCase):
+    def setUp(self):
+        return
+
+    def tearDown(self):
+        return
+
+    if __name__ == '__main__':
+        unittest.main()
